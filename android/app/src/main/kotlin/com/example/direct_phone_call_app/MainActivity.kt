9d8bf80729b742a83eb355c8ee5ab50a7495286e
moseskamira <moses.african@gmail.com>
2023-09-27 13:22:35 +0300

    Set Up IDE

---
+++ android/app/src/main/kotlin/com/example/direct_phone_call_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.direct_phone_call_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/phone_call_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:get/get.dart';
+
+class PhoneCallPage extends StatelessWidget {
+  const PhoneCallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Direct Phone Call'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: Get.width * 0.50),
+              child: ElevatedButton(
+                onPressed: () {
+                  FlutterPhoneDirectCaller.callNumber('705720');
+                },
+                child: const Text(
+                  'Call',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
